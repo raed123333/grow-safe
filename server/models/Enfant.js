f586614460221b4e2139f09780e38bf9ba30730c
JABRI Raed <jabriraed80@gmail.com>
2025-03-06 10:48:16 +0100

    add EnfantModel

---
+++ server/models/Enfant.js
@@ -0,0 +1,34 @@
+const {DataTypes} =require("sequelize");
+const sequelize = require("../config/db");
+
+const Enfant =  sequelize.define("Enfant", {
+        idenf: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nom: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        prenom: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        motpasse: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        idp: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Parents',
+                key: 'idp',
+        },
+        onDelete: "CASCADE", // Si un parent est supprimé, ses enfants le seront aussi
+      
+        },
+    },
+);
+
+module.exports = Enfant;
