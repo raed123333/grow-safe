c55a095f902132ea5f9bef9918c6e1ba090155ce
JABRI Raed <jabriraed80@gmail.com>
2025-03-27 12:20:40 +0100

    add route for loginchild

--- server/routes/enfantRoutes.js
+++ server/routes/enfantRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getEnfants,getEnfantById,createEnfant,updateEnfant,deleteEnfant} = require("../controllers/EnfantController");
+const {getEnfants,getEnfantById,createEnfant,updateEnfant,deleteEnfant,loginEnfant} = require("../controllers/EnfantController");
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/:idenf', getEnfantById);
 router.post('/', createEnfant);
 router.put('/:idenf', updateEnfant);
 router.delete('/:idenf', deleteEnfant);
+router.post('/loginEnfant', loginEnfant);
 
 module.exports = router;
